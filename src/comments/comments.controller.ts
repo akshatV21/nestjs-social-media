3ae8a76d0ad327340e9f15e24103dfe482cb4b2f
akshatV21 <a.vishwakarma2110@gmail.com>
2022-10-21 19:23:40 +0530

    modified

--- src/comments/comments.controller.ts
+++ src/comments/comments.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, Get, Param } from '@nestjs/common'
+import { Controller, Post, Body, Get, Param, UsePipes, ValidationPipe } from '@nestjs/common'
 import { Auth } from 'src/auth/decorators/Auth.decorator'
 import { ReqUser } from 'src/auth/decorators/ReqUser.decorator'
 import { CommentsService } from './comments.service'
@@ -10,24 +10,30 @@ export class CommentsController {
   constructor(private readonly commentsService: CommentsService) {}
 
   @Post()
+  @Auth()
+  @UsePipes(new ValidationPipe())
   async httpCreateNewComment(@Body() commentPayload: CreateComment, @ReqUser() userId: string) {
     const comment = await this.commentsService.create(commentPayload, userId)
     return { success: true, message: 'Comment created successfully', comment: comment }
   }
 
   @Post('reply')
+  @Auth()
+  @UsePipes(new ValidationPipe())
   async httpCreateReplyComment(@Body() commentPayload: ReplyComment, @ReqUser() userId: string) {
     const comment = await this.commentsService.reply(commentPayload, userId)
     return { success: true, message: 'Comment created successfully', comment: comment }
   }
 
   @Get(':id')
+  @Auth()
   async httpGetPostComments(@Param('id') id: string) {
     const comments = await this.commentsService.getPostComments(id)
     return { success: true, message: 'Comments fetched successfully', comments: comments }
   }
 
   @Get('replies/:id')
+  @Auth()
   async httpGetReplies(@Param('id') id: string) {
     const replies = await this.commentsService.getReplies(id)
     return { success: true, message: 'Replies fetched successfully', replies: replies }
